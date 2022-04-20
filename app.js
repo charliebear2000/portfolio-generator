8b91ed0f3387746e810583fdfadf5feebd0e75cf
Leah <amelia_rawls@ccpsnet.net>
2022-04-20 17:22:33 -0400

    added validation

--- app.js
+++ app.js
@@ -16,12 +16,28 @@ const promptUser = () => {
      {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
-      },
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+         if (nameInput) {
+            return true;
+         } else {
+            console.log('Please enter your name!');
+            return false;
+         }
+      }
+   },
       {
          type: 'input',
          name: 'github',
-         message: 'Enter your GitHub Username'
+         message: 'Enter your GitHub Username (Required)',
+         validate: githubInput => {
+            if (githubInput) {
+               return true;
+            } else {
+               console.log('Please enter your GitHub username!');
+               return false;
+            }
+         }
       },
       {
          type: 'input',
@@ -46,36 +62,60 @@ Add a New Project
       {
          type: 'input',
          name: 'name',
-         message: 'What is the name of your project?'
-       },
-       {
+         message: 'What is the name of your project? (Required)',
+         validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project name!');
+              return false;
+            }
+         }
+      },
+      {
          type: 'input',
          name: 'description',
-         message: 'Provide a description of the project (Required)'
-       },
-       {
+         message: 'Provide a description of the project (Required)',
+         validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project description!');
+              return false;
+            }
+         }
+      },
+      {
          type: 'checkbox',
          name: 'languages',
          message: 'What did you build this project with? (Check all that apply)',
          choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-       },
-       {
+      },
+      {
          type: 'input',
          name: 'link',
-         message: 'Enter the GitHub link to your project. (Required)'
-       },
-       {
+         message: 'Enter the GitHub link to your project. (Required)',
+         validate: linkInput => {
+            if (linkInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project GitHub link!');
+              return false;
+            }
+         }
+      },
+      {
          type: 'confirm',
          name: 'feature',
          message: 'Would you like to feature this project?',
          default: false
-       },
-       {
+      },
+      {
          type: 'confirm',
          name: 'confirmAddProject',
          message: 'Would you like to enter another project?',
          default: false
-       }
+      }
    ])
    .then(projectData => {
       portfolioData.projects.push(projectData);
